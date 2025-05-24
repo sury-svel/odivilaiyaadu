fc61ed7c937401cfc9dd2a3b7b927aeab38706d1
Saran Velusamy <reterules@gmail.com>
2025-05-24 13:59:03 -0500

    removed pre-build hooks

--- mobile/eas-build/pre-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "🔐 Rebuilding google-services.json from base64..."
-mkdir -p android/app
-echo "$GOOGLE_SERVICES_JSON_BASE64" | base64 -d > android/app/google-services.json
-ls -l android/app/google-services.json
